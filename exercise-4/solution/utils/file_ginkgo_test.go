12d7f0869fb57bff730bfa12218e02f9c8ddf36e
Benjamin Muschko <benjamin.muschko@gmail.com>
2019-01-23 11:38:50 -0700

    Add different types of tests

---
+++ exercise-4/solution/utils/file_ginkgo_test.go
@@ -0,0 +1,55 @@
+package utils_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/bmuschko/lets-gopher-exercise/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func TestFileUtils(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "File Utils Suite")
+}
+
+var _ = Describe("File Utilities", func() {
+	var newDir string
+
+	BeforeEach(func() {
+		dir, err := ioutil.TempDir("", "test")
+		Expect(err).To(BeNil(), "Failed to create temporary directory %s", dir)
+		defer os.RemoveAll(dir)
+
+		newDir = filepath.Join(dir, "new")
+	})
+
+	Describe("Create Directory", func() {
+		Context("that doesn't exist", func() {
+			It("should be created", func() {
+				err := CreateDir(newDir)
+				Expect(err).To(BeNil(), "Failed to create new directory %s", newDir)
+
+				fileInfo, err := os.Stat(newDir)
+				Expect(err).To(BeNil(), "Expected directory to %s to be created", newDir)
+				Expect(fileInfo.Mode().Perm()).To(Equal(os.FileMode(0755)), "Expected directory permissions 0755 but received %s", fileInfo.Mode().Perm())
+			})
+		})
+
+		Context("that does already exist", func() {
+			It("should not be recreated", func() {
+				err := os.MkdirAll(newDir, 0755)
+				Expect(err).To(BeNil(), "Failed to create new directory %s", newDir)
+				err = CreateDir(newDir)
+				Expect(err).To(BeNil(), "Failed to create new directory %s", newDir)
+
+				fileInfo, err := os.Stat(newDir)
+				Expect(err).To(BeNil(), "Expected directory to %s to be created", newDir)
+				Expect(fileInfo.Mode().Perm()).To(Equal(os.FileMode(0755)), "Expected directory permissions 0755 but received %s", fileInfo.Mode().Perm())
+			})
+		})
+	})
+})
---
+++ exercise-4/solution/utils/file_testify_test.go
@@ -0,0 +1,41 @@
+package utils_test
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	. "github.com/bmuschko/lets-gopher-exercise/utils"
+	. "github.com/stretchr/testify/assert"
+)
+
+func TestCreateDirForNonExistentDirectoryWithTestify(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test")
+	Nil(t, err, "Failed to create temporary directory %s", dir)
+	defer os.RemoveAll(dir)
+
+	newDir := filepath.Join(dir, "new")
+	err = CreateDir(newDir)
+	Nil(t, err, "Failed to create new directory %s", newDir)
+
+	fileInfo, err := os.Stat(newDir)
+	Nil(t, err, "Expected directory to %s to be created", newDir)
+	Equal(t, os.FileMode(0755), fileInfo.Mode().Perm(), "Expected directory permissions 0755 but received %s", fileInfo.Mode().Perm())
+}
+
+func TestCreateDirForExistentDirectoryWithTestify(t *testing.T) {
+	dir, err := ioutil.TempDir("", "test")
+	Nil(t, err, "Failed to create temporary directory %s", dir)
+	defer os.RemoveAll(dir)
+
+	newDir := filepath.Join(dir, "new")
+	err = os.Mkdir(newDir, 0755)
+	Nil(t, err, "Failed to create new directory %s", newDir)
+	err = CreateDir(newDir)
+	Nil(t, err, "Failed to create new directory %s", newDir)
+
+	fileInfo, err := os.Stat(newDir)
+	Nil(t, err, "Expected directory to %s to be created", newDir)
+	Equal(t, os.FileMode(0755), fileInfo.Mode().Perm(), "Expected directory permissions 0755 but received %s", fileInfo.Mode().Perm())
+}
